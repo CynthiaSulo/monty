afc7b0d881a569a2164fb864161f1c59a665dc3a
CynthiaSulo <cynthiasulo2@gmail.com>
2023-07-27 15:03:05 -0400

    correction

--- custom_push.c
+++ custom_push.c
@@ -20,32 +20,32 @@ void custom_push(stack_t **custom_stack, unsigned int custom_line_number)
 		dprintf(2, "Error: malloc failed\n");
 		exit(EXIT_FAILURE);
 	}
-        if (global.tokens[1] == NULL)
-        {
-                free(custom_new_node);
-                dprintf(2, "L%u: usage: push integer\n", custom_line_number);
+	if (global.tokens[1] == NULL)
+	{
+		free(custom_new_node);
+		dprintf(2, "L%u: usage: push integer\n", custom_line_number);
 		exit_status();
-        }
-        if (global.tokens[1][0] == '-')
-        {
-                custom_num_digits++;
-        }
-        while (global.tokens[1][custom_num_digits] != '\0')
-        {
-                if (!isdigit(global.tokens[1][custom_num_digits]))
-                {
-                        free(custom_new_node);
-                        dprintf(2, "L%u: usage: push integer\n", custom_line_number);
-                        exit_status();
-                }
-                custom_num_digits++;
-        }
-        custom_new_node->n = atoi(global.tokens[1]);
-        custom_new_node->prev = NULL;
-        custom_new_node->next = *custom_stack;
-        if (*custom_stack != NULL)
-        {
-                (*custom_stack)->prev = custom_new_node;
-        }
-        *custom_stack = custom_new_node;
+	}
+	if (global.tokens[1][0] == '-')
+	{
+		custom_num_digits++;
+	}
+	while (global.tokens[1][custom_num_digits] != '\0')
+	{
+		if (!isdigit(global.tokens[1][custom_num_digits]))
+		{
+			free(custom_new_node);
+			dprintf(2, "L%u: usage: push integer\n", custom_line_number);
+			exit_status();
+		}
+		custom_num_digits++;
+	}
+	custom_new_node->n = atoi(global.tokens[1]);
+	custom_new_node->prev = NULL;
+	custom_new_node->next = *custom_stack;
+	if (*custom_stack != NULL)
+	{
+		(*custom_stack)->prev = custom_new_node;
+	}
+	*custom_stack = custom_new_node;
 }
